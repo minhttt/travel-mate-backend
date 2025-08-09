711430c221be32e1bd47d4e15432c5a4c1260a5e
minhttt <ascf5649@gmail.com>
2025-08-09 22:56:25 +0700

    config main for deploy

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
   });
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(
+    process.env.PORT ? Number(process.env.PORT) : 3000,
+    '0.0.0.0',
+  );
 }
 bootstrap();
